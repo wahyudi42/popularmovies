2868d9d3b1c0b22d10060af50740a547e50e8da4
Faqih Arifian <faqiharifianaji@gmail.com>
2016-09-28 13:16:56 +0700

    add progres bars to list movie

--- app/src/main/java/id/co/lazystudio/watchIt_freemoviedatabase/adapter/ListMovieAdapter.java
+++ app/src/main/java/id/co/lazystudio/watchIt_freemoviedatabase/adapter/ListMovieAdapter.java
@@ -6,9 +6,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -47,14 +48,35 @@ public class ListMovieAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View v= inflater.inflate(R.layout.item_list_movie, parent, false);
+        final View v= inflater.inflate(R.layout.item_list_movie, parent, false);
 
         Movie movie = mMovieList.get(position);
 
-        ImageView imageView = (ImageView) v.findViewById(R.id.poster_imageview);
-        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) imageView.getLayoutParams();
-        params.height = params.width * 3 / 2;
-        Picasso.with(mContext).load(movie.getPosterPath(mContext, 0)).error(R.drawable.no_image_port).into(imageView);
+
+        final ImageView imageView = (ImageView) v.findViewById(R.id.poster_imageview);
+        imageView.post(new Runnable() {
+            @Override
+            public void run() {
+                RelativeLayout posterRelativeLayout = (RelativeLayout) v.findViewById(R.id.poster_relativelayout);
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, posterRelativeLayout.getWidth() * 3 / 2);
+                imageView.setLayoutParams(params);
+            }
+        });
+
+        Picasso.with(mContext)
+                .load(movie.getPosterPath(mContext, 0))
+                .error(R.drawable.no_image_port)
+                .into(imageView, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        v.findViewById(R.id.poster_progressbar).setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                });
 
         TextView title = (TextView) v.findViewById(R.id.title_textview);
         title.setText(movie.getTitle());
