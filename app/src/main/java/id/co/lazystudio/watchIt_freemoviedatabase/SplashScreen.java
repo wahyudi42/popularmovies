a365e23e6b43d2b555662ad6ae49b94b57c833f8
Faqih Arifian <faqiharifianaji@gmail.com>
2016-10-11 17:13:21 +0700

    splash screen done

--- app/src/main/java/id/co/lazystudio/watchIt_freemoviedatabase/SplashScreen.java
+++ app/src/main/java/id/co/lazystudio/watchIt_freemoviedatabase/SplashScreen.java
@@ -3,12 +3,18 @@ package id.co.lazystudio.watchIt_freemoviedatabase;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
-import android.widget.Toast;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 import id.co.lazystudio.watchIt_freemoviedatabase.connection.TmdbClient;
 import id.co.lazystudio.watchIt_freemoviedatabase.connection.TmdbService;
@@ -17,16 +23,55 @@ import id.co.lazystudio.watchIt_freemoviedatabase.utils.DefaultPreferences;
 import id.co.lazystudio.watchIt_freemoviedatabase.utils.TmdbConfigurationPreference;
 import id.co.lazystudio.watchIt_freemoviedatabase.utils.Utils;
 import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Response;
 
 public class SplashScreen extends AppCompatActivity {
+
+    int colorFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow(); // in Activity's onCreate() for instance
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                colorFilter = getResources().getColor(R.color.colorPrimary, null);
+                getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark, null));
+            }else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                colorFilter = getResources().getColor(R.color.colorPrimary);
+                getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+            }
+        }
+
+        final ImageView background = (ImageView)findViewById(R.id.splash_background);
+
+
+        Picasso.with(this)
+                .load(R.drawable.splash_background)
+                .fit()
+                .centerCrop()
+                .into(background, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        background.setColorFilter(Utils.getColorWithAlpha((float)0.5, colorFilter));
+                        findViewById(R.id.container).setVisibility(View.VISIBLE);
+                        loadData();
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                });
+    }
+
+    private void loadData(){
         final DefaultPreferences defaultPref = new DefaultPreferences(this);
         if(defaultPref.isFirstRun()){
             if(Utils.isInternetConnected(this)) {
@@ -35,7 +80,7 @@ public class SplashScreen extends AppCompatActivity {
 
                 final Call<Configuration> configuration = tmdbService.getConfiguration();
 
-                configuration.enqueue(new Callback<Configuration>() {
+                configuration.enqueue(new retrofit2.Callback<Configuration>() {
                     @Override
                     public void onResponse(Call<Configuration> call, Response<Configuration> response) {
                         TmdbConfigurationPreference pref = new TmdbConfigurationPreference(SplashScreen.this);
@@ -54,25 +99,25 @@ public class SplashScreen extends AppCompatActivity {
                 Dialog dialog = new Dialog(this);
                 dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
                 AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-                alertDialog.setTitle("No Internet");
-                alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                alertDialog.setTitle(getResources().getString(R.string.splash_no_internet_title_dialog));
+                alertDialog.setMessage(getResources().getString(R.string.splash_no_internet_message_dialog));
+                alertDialog.setPositiveButton(getResources().getString(R.string.splash_no_internet_positive_dialog), new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-
-                        Toast.makeText(getApplicationContext(), "OK Button pressed!",
-                                Toast.LENGTH_LONG).show();
-                        dialog.dismiss();
+                        Intent intent = getIntent();
+                        finish();
+                        startActivity(intent);
+                    }
+                });
+                alertDialog.setNegativeButton(getResources().getString(R.string.splash_no_internet_negative_dialog), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
                     }
                 });
                 alertDialog.show();
             }
         }else{
             new Handler().postDelayed(new Runnable() {
-
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
-
                 @Override
                 public void run() {
                     // This method will be executed once the timer is over
@@ -85,7 +130,5 @@ public class SplashScreen extends AppCompatActivity {
                 }
             }, 2000);
         }
-
-
     }
 }
--- app/src/main/java/id/co/lazystudio/watchIt_freemoviedatabase/utils/Utils.java
+++ app/src/main/java/id/co/lazystudio/watchIt_freemoviedatabase/utils/Utils.java
@@ -82,4 +82,10 @@ public class Utils {
 //        int paddingBottom = AdSize.SMART_BANNER.getHeightInPixels(context);
 //        view.setPadding(view.getPaddingLeft(), view.getPaddingTop(), view.getPaddingRight(), paddingBottom);
     }
+
+    public static int getColorWithAlpha(float alpha, int baseColor) {
+        int a = Math.min(255, Math.max(0, (int) (alpha * 255))) << 24;
+        int rgb = 0x00ffffff & baseColor;
+        return a + rgb;
+    }
 }
